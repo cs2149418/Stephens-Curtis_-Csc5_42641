3030db1fa5dcfeb66a39acf33d32282271653ca4
cs2149418 <rizerr@gmail.com>
2017-05-01 22:28:45 -0700

    hmwrk5night1

---
+++ Hmwrk/Assignment_5/Gaddis_8thEd_Chap6_Prob2_RectangleArea/main.cpp
@@ -0,0 +1,67 @@
+/* 
+ * File:   main.cpp
+ * Author: Curtis Stephens
+ * Created in 2017
+ * Purpose:  Function control program to find area of a rectangle
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+float getLength();
+float getWidth();
+float getArea(float, float);
+void displayData(float, float, float);
+//Global Constants
+
+//Function Prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    float length, width, area;
+    //Initialize variables
+    length=getLength();
+    width=getWidth();
+    area=getArea(length, width);
+    //Input data
+    
+    //Map inputs to outputs or process the data
+    
+    //Output the transformed data
+    displayData(length,width,area);
+    //Exit stage right!
+    return 0;
+}
+
+//function for getting length
+float getLength(){
+    float entry;
+    cout<<"Enter Length: ";
+    cin>>entry;
+    return entry;
+}
+//function for getting width
+float getWidth(){
+    float entry;
+    cout<<"Enter Width: ";
+    cin>>entry;
+    return entry;
+}
+
+//Area function
+float getArea(float length, float width)
+{
+    float area=length*width;
+    return area;
+}
+
+//display message
+void displayData(float length, float width, float area)
+{
+    cout<<"The length of the rectangle is "<<length<<endl;
+    cout<<"The width of the rectangle is "<<width<<endl;
+    cout<<"The area of the rectangle is "<<area<<endl;
+}
---
+++ Hmwrk/Assignment_5/Gaddis_8thEd_Chap6_Prob3_WinningDivision/main.cpp
@@ -0,0 +1,78 @@
+/* 
+ * File:   main.cpp
+ * Author: Curtis Stephens
+ * Created 2017
+ * Purpose:  program to determine the winning division
+ */
+
+//System Libraries
+#include <iostream>  //Input - Output Library
+using namespace std; //Name-space under which system libraries exist
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+float getsales();
+float findHighest(float,float,float,float);
+//Execution begins here
+int main(int argc, char** argv) {
+    //Declare variables
+    float a,b,c,d, highest;
+    //Initialize variables
+    
+    //Input data
+    cout<<"How much did the Northeast sale this quarter? $";
+    a=getsales();
+    cout<<"How much did the Northwest sale this quarter? $";
+    b=getsales();
+    cout<<"How much did the Southeast sale this quarter? $";
+    c=getsales();
+    cout<<"How much did the Southwest sale this quarter? $";
+    d=getsales();
+    //Map inputs to outputs or process the data
+    highest=findHighest(a,b,c,d);
+    //Output the transformed data
+    if(highest==a)
+    {
+        cout<<"The Northeastern Division had the greatest sales for this";
+        cout<<" quarter with a total of $"<<a<<endl;
+    }
+    else if(highest==b)
+    {
+        cout<<"The Northwestern Division had the greatest sales for this";
+        cout<<" quarter with a total of $"<<b<<endl;
+    }
+    else if(highest==c)
+    {
+        cout<<"The Southeastern Division had the greatest sales for this";
+        cout<<" quarter with a total of $"<<c<<endl;
+    }
+    else if(highest==d)
+    {
+        cout<<"The Soutwestern Division had the greatest sales for this";
+        cout<<" quarter with a total of $"<<d<<endl;
+    }
+    //Exit stage right!
+    return 0;
+}
+
+//function for getting number and validating input
+float getsales()
+{
+    float entry;
+    cin>>entry;
+    if(entry<0)
+    {
+        cout<<"Invalid amount. Re enter sales: $";
+        cin>>entry;
+    }
+    return entry;
+}
+
+ //function for finding greatest number
+float findHighest(float a, float b, float c, float d)
+{
+    return a>b?a>c?a>d?a:d:c>d?c:d:b>c?b>d?b:d:c>d?c:d;
+}
